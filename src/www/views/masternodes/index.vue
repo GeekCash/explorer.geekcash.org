5657970c3acac03ec6e597306ddbf0cce87cdf1c
Nasa Nguyen <nasa8x@gmail.com>
2018-10-04 00:12:30 +0700

    masternode winner

--- src/www/views/masternodes/index.vue
+++ src/www/views/masternodes/index.vue
@@ -9,28 +9,37 @@
                     <h3 class="panel-title">Masternodes</h3>
                 </header>
                 <div class="panel-body table-responsive">
-                    <mix-table :data="data" css="table table-hover table-striped table-bordered" ref="mixtable" @mixtable:fetch="mnfetch"
-                        :limit=15>
+                    <mix-table :data="data" css="table table-hover table-striped table-bordered" ref="mixtable"
+                        @mixtable:fetch="mnfetch" :limit=15>
+                        <mix-table-column data-field="idx" label="Win at" width="110px"></mix-table-column>
+                        <mix-table-column data-field="wid" label="Addr" type="slot" target="addr"></mix-table-column>
+                        <mix-table-column data-field="stt" label="Status" type="slot" width="15%" target="stt"></mix-table-column>
+                        <mix-table-column data-field="at" label="Active" type="slot" width="15%" target="at"></mix-table-column>
+                        <!-- <mix-table-column data-field="ls" label="Last Seen" type="slot" width="15%" target="ls"></mix-table-column> -->
+                        <mix-table-column data-field="lp" label="Reward" type="slot" width="15%" target="lp"></mix-table-column>
 
-                        <mix-table-column data-field="ip"  label="Address"   width="15%"></mix-table-column>
-                        <mix-table-column data-field="pro" label="Protocol"  width="15%"></mix-table-column>
-                        <mix-table-column data-field="stt" label="Status"    type="slot"   width="15%" target="stt"></mix-table-column>
-                        <mix-table-column data-field="at"  label="Active"    type="slot"   width="15%" target="at"></mix-table-column>
-                        <mix-table-column data-field="ls"  label="Last Seen" type="slot"   width="15%" target="ls"></mix-table-column>
-                        <mix-table-column data-field="wid" label="Payee"     width="25%" ></mix-table-column>
 
                         <template slot="stt" slot-scope="props">
-                            <span v-if="props.value == 'ENABLED'" class="text-success">{{props.value}}</span>
+                            <span v-if="props.value == 'ENABLED' || props.value == 'WATCHDOG_EXPIRED'" class="text-success">{{props.value}}</span>
                             <span v-else class="text-warning">{{props.value}}</span>
                         </template>
 
                         <template slot="at" slot-scope="props">
-                            <span>{{props.value *1000 | duration('humanize') }}</span>
-         
-                       </template>
+                            <span>Last: {{props.row.ls | moment('from','now')}}</span><br>
+                            <span>Active: {{props.value *1000 | duration('humanize') }}</span>
+
+                        </template>
                         <template slot="ls" slot-scope="props">
                             <span>{{props.value | moment('from','now')}}</span>
-                       </template>
+                        </template>
+                        <template slot="lp" slot-scope="props">
+                            <span>Last: {{props.value | moment('from','now')}}</span><br>
+                            <span>Block: {{props.row.lb}}</span>
+                        </template>
+                        <template slot="addr" slot-scope="props">
+                            IP: {{props.row.ip}}<br>
+                            Payee: <router-link :to="{ name: 'address', params: { id: props.value }}">{{props.value}}</router-link>
+                        </template>
                     </mix-table>
 
                 </div>
@@ -43,20 +52,29 @@
 
 
 <script>
-        import MixTable from "v-mix-table";
-        import { mapGetters } from "vuex";
-        
-        export default {
-            computed: {
-                ...mapGetters({
-                    data: "mnfetch",
-                })
-            },
-            methods: {
-                mnfetch(params) { 
-                    this.$store.dispatch("MN_FETCH", params);
-                }
-            },
-            
-        };
-    </script>
+    import MixTable from "v-mix-table";
+    import { mapGetters } from "vuex";
+
+    export default {
+        computed: {
+            ...mapGetters({
+                data: "mnfetch",
+            })
+        },
+        mounted() {
+
+            var _this = this;
+            clearInterval(this.t);
+            this.t = setInterval(function () {
+                _this.$refs.mixtable.reload();
+            }, 60000);
+        },
+
+        methods: {
+            mnfetch(params) {
+                this.$store.dispatch("MN_FETCH", params);
+            }
+        },
+
+    };
+</script>
